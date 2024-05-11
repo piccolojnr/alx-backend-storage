bbda337f2d128d5ef175019df809169913523dd3
piccolojnr <rahimdaud246@gmail.com>
2024-05-11 21:27:40 +0000

    meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,13 @@
+-- Drop the view if it already exists
+DROP VIEW IF EXISTS need_meeting;
+
+-- Create the view need_meeting
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE
+    score < 80 -- Strictly under 80
+    AND (
+        last_meeting IS NULL
+        OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH)
+    );
